09ff53160cb28ba5cc7729060b63cfddf12dc94d
Kevin <kevincpy@gmail.com>
2015-02-09 00:56:05 +0800

    updated tcpclient connection

--- app/build/generated/source/r/debug/com/iems5722/translateapp/R.java
+++ app/build/generated/source/r/debug/com/iems5722/translateapp/R.java
@@ -20,12 +20,12 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080007;
         public static final int button1=0x7f080001;
-        public static final int button2=0x7f080004;
-        public static final int button3=0x7f080005;
-        public static final int button4=0x7f080006;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f080003;
         public static final int input1=0x7f080000;
-        public static final int text1=0x7f080002;
-        public static final int text2=0x7f080003;
+        public static final int text1=0x7f080004;
+        public static final int text2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- app/src/main/java/com/iems5722/translateapp/MainActivity.java
+++ app/src/main/java/com/iems5722/translateapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.iems5722.translateapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -23,51 +30,54 @@ public class MainActivity extends Activity {
     Button b4;
     EditText e1;
     TextView t1;
+    private ProgressDialog pd = null;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// get references to layout objects
-        b1 = (Button)findViewById(R.id.button1);
-        e1 = (EditText)findViewById(R.id.input1);
-        t1 = (TextView)findViewById(R.id.text1);
-        b2 = (Button)findViewById(R.id.button2);
-        b3 = (Button)findViewById(R.id.button3);
-        b4 = (Button)findViewById(R.id.button4);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get references to layout objects
+        b1 = (Button) findViewById(R.id.button1);
+        e1 = (EditText) findViewById(R.id.input1);
+        t1 = (TextView) findViewById(R.id.text1);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
 
-		// add click listener to button to call translateText()
+        // add click listener to button to call translateText()
         b1.setOnClickListener(
-                new View.OnClickListener()
-                {
+                new View.OnClickListener() {
                     public void onClick(View view) {
                         translateText();
                     }
                 }
         );
         b2.setOnClickListener(
-                new View.OnClickListener()
-                {
+                new View.OnClickListener() {
                     public void onClick(View view) {
                         shareResult();
                     }
                 }
         );
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    TcpClient();
+        b3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final ProgressDialog ringProgressDialog = new ProgressDialog(MainActivity.this);
+                        ringProgressDialog.setTitle("Connecting");
+                        ringProgressDialog.show();
+                        ringProgressDialog.setCancelable(false);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TcpClient();
+                                ringProgressDialog.dismiss();
+                            }
+                        }).start();
+                    }
                 }
-                catch (UnknownHostException ex) {
-                    System.out.println(ex);
-                }
-                catch (IOException ex) {
-                    System.out.println(ex);
-                }
-            }
-        });
-	}
+        );
+    }
 
 
     // translate look up
@@ -121,25 +131,44 @@ public class MainActivity extends Activity {
         startActivity(sendIntent);
     }
 	// TCP client
-    private void TcpClient() throws UnknownHostException, IOException{
-        String input = e1.getText().toString();
-        String hostname = "www.google.com";
-        InetAddress serverAddr = InetAddress.getByName(hostname);
+    private void TcpClient() {
+        String hostname = "iems5722v.ie.cuhk.edu.hk";
         int serverPort = 3001;
-        if (serverAddr.isReachable(200)){
-            Toast.makeText(MainActivity.this, "hostname OK", Toast.LENGTH_SHORT).show();
-            return;
+        try {
+            Socket client = new Socket(hostname, serverPort);
+            System.out.println("Just connected to " + client.getRemoteSocketAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        // show some feedback to user: translated text, error message, dialog etc
-        if (input.trim().equals("")) {
-            Toast.makeText(MainActivity.this, "Empty Input", Toast.LENGTH_SHORT).show();
-            return;
-        }
-       // Toast.makeText(MainActivity.this, hostname +"="+ serverAddr, Toast.LENGTH_SHORT).show();
-        //Socket client = new Socket(serverAddr, serverPort);
-        //System.out.println("Just connected to" + client.getRemoteSocketAddress());
     }
+//            try {
+//
+//                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+//                out.writeUTF(input + client.getLocalSocketAddress());
+//                DataInputStream in = new DataInputStream(client.getInputStream());
+//                Toast.makeText(MainActivity.this, "Translate Result: " + in.readUTF(), Toast.LENGTH_SHORT).show();
+//                client.close();
+//            }
+
+
+
+
+
+//        try {
+//            client = new Socket(serverAddr, serverPort);
+//
+//            alertDialogBuilder.setMessage("Just connected to" + client.getRemoteSocketAddress());
+//        }
+//        catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
 	// Options menu - not needed for this app
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
