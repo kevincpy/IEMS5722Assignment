516a7a5aad9211cbd177948c296f2ee743ea18dd
Kevin <kevincpy@gmail.com>
2015-02-10 01:03:56 +0800

    completed http mode fixed some empty input bugs

--- app/src/main/java/com/iems5722/translateapp/MainActivity.java
+++ app/src/main/java/com/iems5722/translateapp/MainActivity.java
@@ -1,9 +1,7 @@
 package com.iems5722.translateapp;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,27 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.AlertDialog;
+
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.io.IOException;
@@ -240,9 +230,6 @@ public class MainActivity extends Activity {
 
             }
             return null;
-        }
-
-        protected void onProgressUpdate(){
         }
 
         protected void onPostExecute(String result){
