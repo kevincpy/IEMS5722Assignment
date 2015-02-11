d435ee594c3c4d0d6b047143d6946e09fcc9e880
kevincpy <kevincpy@gmail.com>
2015-02-11 10:47:41 +0800

    Added Connection Detectoer class and completed the internet connection checking

--- app/src/main/java/com/iems5722/translateapp/MainActivity.java
+++ app/src/main/java/com/iems5722/translateapp/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity {
     TextView t1;
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // get references to layout objects
@@ -98,17 +98,17 @@ public class MainActivity extends Activity {
             return;
         }
 
-        if(word != null){
+        if (word != null) {
             t1.setText(word);
             return;
-        }
-        else {
+        } else {
             Toast.makeText(MainActivity.this, "Translate Error", Toast.LENGTH_SHORT).show();
             t1.setText("Translate Error");
             return;
         }
 
     }
+
     // share the translate result
     private void shareResult() {
         String input = e1.getText().toString();
@@ -120,23 +120,21 @@ public class MainActivity extends Activity {
             return;
         }
 
-        if(!word.isEmpty()){
+        if (!word.isEmpty()) {
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Translate result is " + "\" " + input + " -> " + word +" \" !" + "\n\n### From TranslateApp ### ");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Translate result is " + "\" " + input + " -> " + word + " \" !" + "\n\n### From TranslateApp ### ");
             sendIntent.setType("text/plain");
             startActivity(sendIntent);
-        }
-        else {
+        } else {
             Toast.makeText(MainActivity.this, "Please press Translate", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
-
     }
 
-    private void TranslateJob(String mode){
+    private void TranslateJob(String mode) {
         String input = e1.getText().toString();
 
         new TranslateTask().execute(mode, input);
@@ -144,29 +142,30 @@ public class MainActivity extends Activity {
     }
 
 
-	// Options menu - not needed for this app
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    // Options menu - not needed for this app
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private class TranslateTask extends AsyncTask<String, Void, String> {
         //final ProgressDialog ringProgressDialog = new ProgressDialog(MainActivity.this);
-        protected String doInBackground(String... Strings){
+        protected String doInBackground(String... Strings) {
+            ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
             String mode = Strings[0];
             String input = Strings[1];
             String ServerResponse = "";
@@ -174,9 +173,16 @@ public class MainActivity extends Activity {
             if (input.trim().equals("")) {
                 return "Empty Input";
             }
-            System.out.println("Mode: " + mode);
 
-            if (mode.equals("tcp")){
+
+            Boolean isInternetPresent = cd.isConnectingToInternet();
+            if (!isInternetPresent){
+                System.out.println("Can't connect to internet");
+                return "Can't connect to internet";
+
+            }
+            System.out.println("Mode: " + mode);
+            if (mode.equals("tcp")) {
                 String hostname = "iems5722v.ie.cuhk.edu.hk";
                 int serverPort = 3001;
                 System.out.println("Try connected to " + hostname + " : " + serverPort);
@@ -199,31 +205,27 @@ public class MainActivity extends Activity {
                             return ServerResponse;
                         }
                     }
-                }
-                catch (UnknownHostException e) {
+                } catch (UnknownHostException e) {
                     e.printStackTrace();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             }
-            if (mode.equals("http")){
+            if (mode.equals("http")) {
                 String url = "http://iems5722v.ie.cuhk.edu.hk:8080/translate.php?word=" + input;
                 System.out.println("URL: " + url);
                 HttpClient http_client = new DefaultHttpClient();
                 HttpGet request = new HttpGet(url);
-                try{
+                try {
                     HttpResponse response = http_client.execute(request);
                     System.out.println("Get Server response!");
                     HttpEntity entity = response.getEntity();
                     ServerResponse = EntityUtils.toString(entity, HTTP.UTF_8);
                     return ServerResponse;
-                }
-                catch (UnknownHostException e) {
+                } catch (UnknownHostException e) {
                     e.printStackTrace();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
@@ -232,8 +234,8 @@ public class MainActivity extends Activity {
             return null;
         }
 
-        protected void onPostExecute(String result){
-            if (result.equals("Empty Input")) {
+        protected void onPostExecute(String result) {
+            if (result.equals("Empty Input") || result.equals("Can't connect to internet") ) {
                 Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -242,7 +244,6 @@ public class MainActivity extends Activity {
                 t1.setText(result);
                 return;
             }
-
             t1.setText(result);
             return;
 
@@ -250,5 +251,3 @@ public class MainActivity extends Activity {
     }
 
 }
-
-
